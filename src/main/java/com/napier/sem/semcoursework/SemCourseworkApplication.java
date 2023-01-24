10dabaf87ef24f0496f4fd746bee4c4182e537f8
Joe Robinson <joelukerobinson@googlemail.com>
2023-01-24 21:23:18 +0000

    Project setup and database flyway migration

---
+++ src/main/java/com/napier/sem/semcoursework/SemCourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.napier.sem.semcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemCourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemCourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/napier/sem/semcoursework/SemCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napier.sem.semcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemCourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
