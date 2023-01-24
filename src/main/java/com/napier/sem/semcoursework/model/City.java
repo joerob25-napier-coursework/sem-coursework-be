568cb45489cb0c8393ab63c47e23291d82e38b9f
Joe Robinson <joelukerobinson@googlemail.com>
2023-01-24 22:07:34 +0000

    added sample endpoint

---
+++ src/main/java/com/napier/sem/semcoursework/model/City.java
@@ -0,0 +1,27 @@
+package com.napier.sem.semcoursework.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "country_code")
+    private String countryCode;
+    @Column(name = "district")
+    private String district;
+    @Column(name = "population")
+    private int population;
+}
