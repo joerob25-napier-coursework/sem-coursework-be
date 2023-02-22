874403e808446c340e3c0de72146944d38a30e79
Sommie999 <40627853@live.napier.ac.uk>
2023-02-22 09:06:23 +0000

    amended sql query

--- src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
+++ src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
@@ -9,8 +9,10 @@ import java.util.List;
 
 public interface PopulationRepository extends JpaRepository<Population, Integer> {
 
-    @Query(value="select continent, population, CONCAT(ROUND((city.population/population)*100,2),'%') AS in_cities," +
-            "(population-in_cities)*100 AS not_in_cities from country JOIN city ON country.code=city.country_code",
+    @Query(value="select continent, country.Population, CONCAT(ROUND((city.population/country.population)*100,2),'%') AS in_cities, CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%') AS not_in_cities\n" +
+            "from country\n" +
+            "JOIN city ON country.code=city.CountryCode\n" +
+            "GROUP BY continent",
             nativeQuery = true)
     List<Population> populationCitiesInContinent();
 }
