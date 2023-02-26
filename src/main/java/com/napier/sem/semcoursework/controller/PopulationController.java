3087d3ac1d41f8791e236c48376648a7a2c09dea
Sommie999 <40627853@live.napier.ac.uk>
2023-02-26 17:43:09 +0000

    updated population controller

--- src/main/java/com/napier/sem/semcoursework/controller/PopulationController.java
+++ src/main/java/com/napier/sem/semcoursework/controller/PopulationController.java
@@ -1,2 +1,24 @@
-package com.napier.sem.semcoursework.controller;public class PopulationController {
+package com.napier.sem.semcoursework.controller;
+
+import com.napier.sem.semcoursework.model.City;
+import com.napier.sem.semcoursework.repository.CityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Controller for handling requests related to population.
+ */
+@RestController
+@RequestMapping("/population")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class PopulationController {
 }
