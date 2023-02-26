46f2635a67e6603a5fd57e82adc6b21a4028346f
Sommie999 <40627853@live.napier.ac.uk>
2023-02-26 15:18:53 +0000

    sorted sql connectivity and updates populationrepostiry with comments

--- src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
+++ src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
@@ -1,3 +1,8 @@
+/**
+
+ * The PopulationRepository interface provides methods for accessing and modifying population data in the database.
+ */
+
 package com.napier.sem.semcoursework.repository;
 
 import com.napier.sem.semcoursework.model.Country;
@@ -7,12 +12,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PopulationRepository extends JpaRepository<Population, Integer> {
+/**
+ * Queries the database to retrieve population data for each continent, including the total population, the percentage of
+ * the population living in cities, and the percentage of the population not living in cities.
+ * return a list of Population objects containing the requested data
+ */
 
+public interface PopulationRepository extends JpaRepository<Population, Integer> {
     @Query(value="select continent, country.Population, CONCAT(ROUND((city.population/country.population)*100,2),'%') AS in_cities, CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%') AS not_in_cities\n" +
             "from country\n" +
             "JOIN city ON country.code=city.CountryCode\n" +
             "GROUP BY continent",
             nativeQuery = true)
     List<Population> populationCitiesInContinent();
+
 }
