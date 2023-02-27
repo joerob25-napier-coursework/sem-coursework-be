f8fb2c872671ef85ddd4ce539354687682921365
Sommie999 <40627853@live.napier.ac.uk>
2023-02-27 20:59:59 +0000

    got sql code working, building select

--- src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
+++ src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
@@ -1,5 +1,6 @@
 package com.napier.sem.semcoursework.repository;
 
+import com.napier.sem.semcoursework.model.Country;
 import com.napier.sem.semcoursework.model.Population;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,10 +14,13 @@ import java.util.List;
 public class PopulationRepository {
     private final JdbcTemplate template;
     public List<Population> populationCitiesInContinent() {
-        return template.query(
-                "select continent, ANY_VALUE(country.Population), ANY_VALUE(CONCAT(ROUND((city.population/country.population)*100,2),'%')) AS in_cities, ANY_VALUE(CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%')) AS not_in_cities\n" +
-                "from country\n" +
-                        "JOIN city ON country.code=city.CountryCode\n" +
-                        "GROUP BY continent",(rs,rowNum)-> Population.builder().continent(rs.getString("continent")).population(rs.getString("country.Population")).in_cities(rs.getString("in_cities")).not_in_cities(rs.getString("not_in_cities")).build());
+        try {
+            return template.query(
+                    "select continent, country.Population from country", (rs,rowNum) -> Population.builder().continent(rs.getString("continent")).population(rs.getString("country.Population")).build());
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            return null;
+        }
+
     }
 }
