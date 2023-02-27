e355b17116bd6e6ddfe3040cca4645bdc99b052c
Sommie999 <40627853@live.napier.ac.uk>
2023-02-27 10:29:18 +0000

    added ANY_VALUE to the select to allow GROUP BY that is incompatible with sql_mode=only_full_group_by

--- src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
+++ src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
@@ -14,7 +14,7 @@ public class PopulationRepository {
     private final JdbcTemplate template;
     public List<Population> populationCitiesInContinent() {
         return template.query(
-                "select continent, country.Population, CONCAT(ROUND((city.population/country.population)*100,2),'%') AS in_cities, CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%') AS not_in_cities\n" +
+                "select continent, ANY_VALUE(country.Population), ANY_VALUE(CONCAT(ROUND((city.population/country.population)*100,2),'%')) AS in_cities, ANY_VALUE(CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%')) AS not_in_cities\n" +
                 "from country\n" +
                         "JOIN city ON country.code=city.CountryCode\n" +
                         "GROUP BY continent",(rs,rowNum)-> Population.builder().continent(rs.getString("continent")).population(rs.getString("country.Population")).in_cities(rs.getString("in_cities")).not_in_cities(rs.getString("not_in_cities")).build());
