e83ee0fb9fb73ed1f20330fbe3b2b2cdd0dd5f92
Sommie999 <40627853@live.napier.ac.uk>
2023-02-27 21:47:16 +0000

    got sql code working, building select - group by not working

--- src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
+++ src/main/java/com/napier/sem/semcoursework/repository/PopulationRepository.java
@@ -16,7 +16,7 @@ public class PopulationRepository {
     public List<Population> populationCitiesInContinent() {
         try {
             return template.query(
-                    "select continent, country.Population, CONCAT(ROUND((city.population/country.population)*100,2),'%') AS in_cities, CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%') AS not_in_cities FROM country JOIN city ON country.code=city.country_code GROUP BY country.continent", (rs,rowNum) -> Population.builder().continent(rs.getString("continent")).population(rs.getString("country.Population")).in_cities(rs.getString("in_cities")).not_in_cities(rs.getString("not_in_cities")).build());
+                    "select continent, country.Population, CONCAT(ROUND((city.population/country.population)*100,2),'%') AS in_cities, CONCAT(ROUND((((country.Population)-(city.Population))/(country.Population))*100,2),'%') AS not_in_cities FROM country JOIN city ON country.code=city.country_code GROUP BY continent", (rs,rowNum) -> Population.builder().continent(rs.getString("continent")).population(rs.getString("country.Population")).in_cities(rs.getString("in_cities")).not_in_cities(rs.getString("not_in_cities")).build());
         }catch (Exception e){
             System.out.println(e.getLocalizedMessage());
             return null;
