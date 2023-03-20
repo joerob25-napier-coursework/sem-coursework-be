006525f528ef5b07f0651e9bf50786d0d4ba58c2
Sommie999 <40627853@live.napier.ac.uk>
2023-03-20 18:57:15 +0000

    added v-region-population table view and new report

--- src/main/resources/db/migration/V3__CreatePopulationView.sql
+++ src/main/resources/db/migration/V3__CreatePopulationView.sql
@@ -19,3 +19,25 @@ from continents_populations copops
          left join cities_populations cipops on (copops.code = cipops.country_code)
 group by copops.continent
 );
+
+DROP VIEW IF EXISTS `v_region_population`;
+CREATE VIEW `v_region_population`(qID, population, in_cities, not_in_cities) as (
+with region_populations as (
+    select code, region, sum(population) as region_population
+    from country
+    group by code, region
+),
+     cities_populations as (
+         select country_code, sum(population) as city_population
+         from city
+         group by country_code
+     )
+select
+    regpops.region,
+    coalesce(sum(regpops.region_population),0) as total_region_population,
+    coalesce(sum(cipops.city_population),0) as total_in_city_population,
+    coalesce(sum((regpops.region_population - cipops.city_population)),0) as total_not_in_city_population
+from region_populations regpops
+         left join cities_populations cipops on (regpops.code = cipops.country_code)
+group by regpops.region
+);
