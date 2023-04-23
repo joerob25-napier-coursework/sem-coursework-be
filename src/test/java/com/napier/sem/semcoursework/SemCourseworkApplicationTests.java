c00e3ec1715193270d0070e091c5cb32568fb03f
Joe Robinson <joelukerobinson@googlemail.com>
2023-04-23 20:07:34 +0100

    added unit profile

--- src/test/java/com/napier/sem/semcoursework/SemCourseworkApplicationTests.java
+++ src/test/java/com/napier/sem/semcoursework/SemCourseworkApplicationTests.java
@@ -3,6 +3,7 @@ package com.napier.sem.semcoursework;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,13 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Ensure that Spring is configured correctly and database connection to run the flyway
  */
 
-@Slf4j
+
+@ActiveProfiles("unit")
 @SpringBootTest
 class SemCourseworkApplicationTests {
 
 	@Test
 	void contextLoads() {
-		log.info("Running Flyway database migration");
 		assertTrue(true);
 	}
 }
